a947cd31bb6477408dd38c67a687d8f0da525e36
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-09-20 20:32:01 +0200

    Add test entities

--- sources/PrototypeGame.cpp
+++ sources/PrototypeGame.cpp
@@ -42,9 +42,11 @@
 #include "systems/ParticuleSystem.h"
 #include "systems/ScrollingSystem.h"
 #include "systems/MorphingSystem.h"
+#include "systems/AutonomousAgentSystem.h"
 
 #include <cmath>
 
+static Entity e, cursor, wander;
 
 static void updateFps(float dt);
 
@@ -59,7 +61,41 @@ void PrototypeGame::init(const uint8_t* in, int size) {
 	loadFont(asset, "typo");
 	
 	PlacementHelper::GimpWidth = 0; // TODO
-    PlacementHelper::GimpHeight = 0; // TODO	
+    PlacementHelper::GimpHeight = 0; // TODO
+    
+    cursor = theEntityManager.CreateEntity();
+    ADD_COMPONENT(cursor, Transformation);
+    TRANSFORM(cursor)->size = Vector2(0.2, 0.2);
+    ADD_COMPONENT(cursor, Rendering);
+    RENDERING(cursor)->hide = false;
+ 
+    wander = theEntityManager.CreateEntity();
+    ADD_COMPONENT(wander, Transformation);
+    TRANSFORM(wander)->size = Vector2(0.1, 0.1);
+    ADD_COMPONENT(wander, Rendering);
+    RENDERING(wander)->color = Color(0.5, 0.5, 0.8);
+    RENDERING(wander)->hide = false;
+       
+    e = theEntityManager.CreateEntity();
+    ADD_COMPONENT(e, Transformation);
+    TRANSFORM(e)->size = Vector2(0.2, 0.2);
+    ADD_COMPONENT(e, Rendering);
+    RENDERING(e)->hide = false;
+    ADD_COMPONENT(e, Physics);
+    PHYSICS(e)->mass = 1;
+    ADD_COMPONENT(e, AutonomousAgent);
+    AUTONOMOUS_AGENT(e)->maxSpeed = 2;
+    AUTONOMOUS_AGENT(e)->maxForce = 100;
+    AUTONOMOUS_AGENT(e)->seekTarget = 0;
+    AUTONOMOUS_AGENT(e)->seekWeight = 10;
+    AUTONOMOUS_AGENT(e)->fleeTarget = 0;
+    AUTONOMOUS_AGENT(e)->fleeWeight = 10;
+    AUTONOMOUS_AGENT(e)->fleeRadius = 1;
+    AUTONOMOUS_AGENT(e)->arriveDeceleration = 0.3;
+	AUTONOMOUS_AGENT(e)->wander.radius = 0.5;
+	AUTONOMOUS_AGENT(e)->wander.distance = 1;
+	AUTONOMOUS_AGENT(e)->wander.jitter = 0.1;
+	AUTONOMOUS_AGENT(e)->wander.target = Vector2::UnitX;
 }
 
 
@@ -72,6 +108,21 @@ void PrototypeGame::togglePause(bool activate) {
 
 void PrototypeGame::tick(float dt) {
 	theTouchInputManager.Update(dt);
+	
+	if (theTouchInputManager.isTouched()) {
+		TRANSFORM(cursor)->position = theTouchInputManager.getTouchLastPosition();
+		
+		RENDERING(cursor)->color = Color(0, 1, 0);
+		AUTONOMOUS_AGENT(e)->seekTarget = cursor;
+		AUTONOMOUS_AGENT(e)->fleeTarget = 0;
+		AUTONOMOUS_AGENT(e)->wanderWeight = 0;
+	} else {
+		RENDERING(cursor)->color = Color(1, 0, 0);
+		AUTONOMOUS_AGENT(e)->seekTarget = 0;
+		AUTONOMOUS_AGENT(e)->fleeTarget = cursor;
+		AUTONOMOUS_AGENT(e)->wanderWeight = 10;
+		TRANSFORM(wander)->position = AUTONOMOUS_AGENT(e)->wander.debugTarget;
+	}
 
     // systems update
 	theADSRSystem.Update(dt);
@@ -82,6 +133,7 @@ void PrototypeGame::tick(float dt) {
 	theScrollingSystem.Update(dt);
 	theContainerSystem.Update(dt);
 	theTextRenderingSystem.Update(dt);
+	theAutonomousAgentSystem.Update(dt);
 	theSoundSystem.Update(dt);
     theMusicSystem.Update(dt);
     theTransformationSystem.Update(dt);
