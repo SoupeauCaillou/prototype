b15b4e6dc363c18cdab9b2cffc40bd3cca0c81fe
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2012-09-25 22:53:57 +0200

    blindmaze prototype

--- sources/PrototypeGame.cpp
+++ sources/PrototypeGame.cpp
@@ -42,7 +42,7 @@
 #include "systems/ParticuleSystem.h"
 #include "systems/ScrollingSystem.h"
 #include "systems/MorphingSystem.h"
-
+#include <iomanip>
 #include <cmath>
 #include <vector>
 
@@ -50,42 +50,11 @@
 #include <GL/glfw.h>
 #endif
 
-Entity background;
-std::vector<Entity> decors, murs, sols;
-int activeIndex = -1;
-std::vector<Entity> monstrons;
-
-Entity zolon;
-
-#define SOL 0.0
-#define MUR 1.0
-
-float decorsDef[] = {
--2.025, -0.875, 0.617455, 1.09657, 0.466667, MUR,
--2, -1.7, 0.482553, 1.32046, -0.433333, MUR,
--2.6, -3.175, 1, 2.39753, 0, MUR,
-
-0.75, -4.5, 7.37226, 1, 0.0333333, SOL, 
-
-4.4, -2.9, 0.698124, 3.53807, -0.2, MUR,
-4.5, -0.0500002, 0.84981, 3.43235, 0.2, MUR,
-
-5.1, 1.275, 2.60176, 0.764209, 0, SOL,
-7.35, 0.0749998, 3.60588, 0.831206, -0.8, SOL, 
-9.25, -1.375, 3.66604, 1, 0,SOL,
--8.175, -0.375, 6.47703, 1, -0.166667, SOL,
--4.3, -0.8, 4.68243, 1, 0,SOL,
--9.5, 0.275, 3.66975, 1, 0,SOL,
-
--7.95, 0.0749998, 0.815077, 2.87012, 0, MUR,
--10.275, 1.65, 0.815077, 2.87012, 0, MUR
-};
-
-float monstresDef[] = {
--0.65, -1.075, 0.76271, 0.76271, 
--4.6, 0.375, 0.76271, 0.76271, 
-2.8, 0.475, 0.76271, 0.76271,
-};
+std::vector<Entity> obstacles;
+Entity depart, arrivee, player, startButton, chrono;
+float startTime;
+bool playing;
+int obstacleCount = 30;
 
 static void updateFps(float dt);
 
@@ -93,72 +62,137 @@ PrototypeGame::PrototypeGame(AssetAPI* ast, NameInputAPI* inputUI, LocalizeAPI* 
 	asset = ast;
 	exitAPI = exAPI;
 }
-void PrototypeGame::init(const uint8_t* in, int size) {    
-	theRenderingSystem.loadAtlas("alphabet", true);   
-    theRenderingSystem.loadAtlas("decor", true);   
 
-	// init font
-	loadFont(asset, "typo");
-	
-	PlacementHelper::GimpWidth = 800;
-    PlacementHelper::GimpHeight = 400;
+static void resetGame() {
+    TEXT_RENDERING(startButton)->hide = false;
+    CONTAINER(startButton)->enable = true;
+    RENDERING(startButton)->hide = false;
+    BUTTON(startButton)->enabled = true;
+    // RENDERING(player)->hide = true;
+    playing = false;
+}
 
-    background = theEntityManager.CreateEntity();
-    ADD_COMPONENT(background, Transformation);
-    TRANSFORM(background)->size = Vector2(PlacementHelper::ScreenWidth, PlacementHelper::ScreenHeight);
-    TRANSFORM(background)->z = 0.1;
-    ADD_COMPONENT(background, Rendering);
-    RENDERING(background)->texture = theRenderingSystem.loadTextureFile("background");
-    RENDERING(background)->hide = false;
-    
-    for (int i=0; i<14; i++) {
+static bool collideWithAnotherObstacle(TransformationComponent* tc) {
+    for (std::vector<Entity>::iterator it=obstacles.begin(); it!=obstacles.end(); ++it) {
+        TransformationComponent* tc2 = TRANSFORM(*it);
+        if (IntersectionUtil::rectangleRectangle(tc->position, tc->size * 1.4, tc->rotation,
+            tc2->position, tc2->size * 1.4, tc2->rotation)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static void initGame() {
+    for (int i=0; i<obstacles.size(); i++) {
+        theEntityManager.DeleteEntity(obstacles[i]);
+    }
+    obstacles.clear();
+
+    float minX = 0, maxX = 0;
+    for (int i=0; i<obstacleCount; i++) {
         Entity e = theEntityManager.CreateEntity();
         ADD_COMPONENT(e, Transformation);
-        TRANSFORM(e)->position = Vector2(decorsDef[6*i], decorsDef[6*i+1]);
-        TRANSFORM(e)->size = Vector2(decorsDef[6*i+2], decorsDef[6*i+3]);
-        TRANSFORM(e)->rotation = decorsDef[6*i+4];
+        TransformationComponent* tc = TRANSFORM(e);
+        do {
+            tc->size = Vector2(1.7, 0.4);
+            tc->position = Vector2(
+                (MathUtil::RandomFloat() -0.5 )* PlacementHelper::ScreenWidth,
+                (MathUtil::RandomFloat() -0.5 )* PlacementHelper::ScreenHeight);
+            tc->rotation = MathUtil::RandomFloat() * 6.28;
+        } while (collideWithAnotherObstacle(tc));
+
         TRANSFORM(e)->z = 0.5;
         ADD_COMPONENT(e, Rendering);
         RENDERING(e)->color = Color::random();
         RENDERING(e)->color.a = 0.5;
         RENDERING(e)->hide = false;
+        obstacles.push_back(e);
         
-        if (decorsDef[6*i+5] == MUR)
-            murs.push_back(e);
-        else
-            sols.push_back(e);
-        
-        decors.push_back(e);
+        if (TRANSFORM(e)->position.X < minX) {
+            depart = e;
+            minX = TRANSFORM(e)->position.X;
+        }
+        else if (TRANSFORM(e)->position.X > maxX) {
+            arrivee = e;
+            maxX = TRANSFORM(e)->position.X;
+        }
     }
-    zolon = theEntityManager.CreateEntity();
-    ADD_COMPONENT(zolon, Transformation);
-    TRANSFORM(zolon)->position = Vector2(-9, 2);
-    TRANSFORM(zolon)->size = Vector2(0.572173, 0.815538);
-    TRANSFORM(zolon)->rotation = 0;
-    TRANSFORM(zolon)->z = 0.8;
-    ADD_COMPONENT(zolon, Rendering);
-    RENDERING(zolon)->texture = theRenderingSystem.loadTextureFile("zolon");
-    RENDERING(zolon)->hide = false;
-    ADD_COMPONENT(zolon, Physics);
-    PHYSICS(zolon)->mass = 1;
-    PHYSICS(zolon)->gravity = Vector2(0, -10);
+    TRANSFORM(depart)->size = TRANSFORM(arrivee)->size = Vector2(1,1);
+    RENDERING(depart)->color = Color(0, 1, 0);
+    RENDERING(arrivee)->color = Color(1, 0, 0);
     
+    TRANSFORM(player)->position = TRANSFORM(depart)->position;
+    TEXT_RENDERING(startButton)->hide = true;
+    CONTAINER(startButton)->enable = false;
+    RENDERING(startButton)->hide = true;
+    BUTTON(startButton)->enabled = false;
+    RENDERING(player)->hide = false;
+    TEXT_RENDERING(chrono)->hide = false;
+    
+    ADD_COMPONENT(obstacles[0], ADSR);
+    ADSR(obstacles[0])->idleValue = 0;
+    ADSR(obstacles[0])->attackValue = 1;
+    ADSR(obstacles[0])->attackTiming = 1;
+    ADSR(obstacles[0])->sustainValue = 1;
+    ADSR(obstacles[0])->decayTiming = 0;
+    ADSR(obstacles[0])->releaseTiming = 0;
+    
+    startTime = TimeUtil::getTime();
+    playing = true;
+}
 
-    for (int i=0; i<3;i ++) {
-        Entity e = theEntityManager.CreateEntity();
-        ADD_COMPONENT(e, Transformation);
-        TRANSFORM(e)->position = Vector2(monstresDef[4*i], monstresDef[4*i+1]);
-        TRANSFORM(e)->size = Vector2(monstresDef[4*i+2], monstresDef[4*i+3]);
-        TRANSFORM(e)->z = 0.6;
-        ADD_COMPONENT(e, Rendering);
-        RENDERING(e)->texture = theRenderingSystem.loadTextureFile("monstron");
-        RENDERING(e)->hide = false;
-        monstrons.push_back(e);
-    }
+void PrototypeGame::init(const uint8_t* in, int size) {
+	theRenderingSystem.loadAtlas("alphabet", true);
+
+	// init font
+	loadFont(asset, "typo");
+	
+	PlacementHelper::GimpWidth = 640;
+    PlacementHelper::GimpHeight = 480;
+
+    Entity bg = theEntityManager.CreateEntity();
+    ADD_COMPONENT(bg, Transformation);
+    TRANSFORM(bg)->size = Vector2(PlacementHelper::ScreenWidth, PlacementHelper::ScreenHeight);
+    TRANSFORM(bg)->z = 0.05;
+    ADD_COMPONENT(bg, Rendering);
+    RENDERING(bg)->hide = false;
+    RENDERING(bg)->color = Color(0.5, 0.5, 0.5);
+    
+    player = theEntityManager.CreateEntity();
+    ADD_COMPONENT(player, Transformation);
+    TRANSFORM(player)->size = Vector2(0.5,0.5);
+    TRANSFORM(player)->z = 0.7;
+    ADD_COMPONENT(player, Rendering);
+    
+    chrono = theEntityManager.CreateEntity();
+    ADD_COMPONENT(chrono, Transformation);
+    TRANSFORM(chrono)->position = Vector2(0, PlacementHelper::GimpYToScreen(30));
+    TRANSFORM(chrono)->z = 0.8;
+    ADD_COMPONENT(chrono, TextRendering);
+    TEXT_RENDERING(chrono)->charHeight = 1;
+    TEXT_RENDERING(chrono)->positioning = TextRenderingComponent::CENTER;
+
+    startButton = theEntityManager.CreateEntity();
+    ADD_COMPONENT(startButton, Transformation);
+    TRANSFORM(startButton)->position = Vector2::Zero;
+    TRANSFORM(startButton)->z = 0.9;
+    ADD_COMPONENT(startButton, TextRendering);
+    TEXT_RENDERING(startButton)->text = "Jouer";
+    TEXT_RENDERING(startButton)->charHeight = 1;
+    ADD_COMPONENT(startButton, Container);
+    CONTAINER(startButton)->entities.push_back(startButton);
+    CONTAINER(startButton)->includeChildren = true;
+    ADD_COMPONENT(startButton, Rendering);
+    RENDERING(startButton)->color = Color(0.2, 0.2, 0.2, 0.5);
+    ADD_COMPONENT(startButton, Button);
+    
+    resetGame();
 }
 
 
 void PrototypeGame::backPressed() {
+#if 0
 #ifndef EMSCRIPTEN
     Entity e = theEntityManager.CreateEntity();
     ADD_COMPONENT(e, Transformation);
@@ -172,9 +206,11 @@ void PrototypeGame::backPressed() {
     decors.push_back(e);
     activeIndex = decors.size() - 1;
 #endif
+#endif
 }
 
 void PrototypeGame::togglePause(bool activate) {
+#if 0
 #ifndef EMSCRIPTEN
     if (activeIndex >= 0) {
         Entity e = decors[activeIndex];
@@ -183,26 +219,12 @@ void PrototypeGame::togglePause(bool activate) {
         activeIndex = -1;
     }
 #endif
+#endif
 }
-
-void entityToLinePoints(Entity e, Vector2& a, Vector2& b, bool top) {
-    const Vector2& size = TRANSFORM(e)->size;
-    const Vector2& pos = TRANSFORM(e)->worldPosition;
-    const float rot =  TRANSFORM(e)->rotation;
-    if (top) {
-        a = pos + Vector2::Rotate(Vector2(-size.X * 0.5, size.Y * 0.5), rot);
-        b = pos + Vector2::Rotate(Vector2(size.X * 0.5, size.Y * 0.5), rot);
-    } else {
-        a = pos + Vector2::Rotate(Vector2(-size.X * 0.5, -size.Y * 0.5), rot);
-        b = pos + Vector2::Rotate(Vector2(size.X * 0.5, -size.Y * 0.5), rot);
-    }
-}
-
-float zolonSpeed = 1;
 
 void PrototypeGame::tick(float dt) {
 	theTouchInputManager.Update(dt);
- 
+#if 0
 #ifndef EMSCRIPTEN
     if (activeIndex >= 0) {
         Entity e = decors[activeIndex];
@@ -244,46 +266,52 @@ void PrototypeGame::tick(float dt) {
         }
     }
 #endif
+#endif
 
-    if (TRANSFORM(zolon)->parent) {
-        // check we are still on platform
-        Entity plat = TRANSFORM(zolon)->parent;
-        if (IntersectionUtil::rectangleRectangle(
-                TRANSFORM(zolon)->worldPosition, TRANSFORM(zolon)->size, TRANSFORM(zolon)->rotation,
-                TRANSFORM(plat)->position, TRANSFORM(plat)->size, TRANSFORM(plat)->rotation)) {
-            // move along platform
-            TRANSFORM(zolon)->position.X += zolonSpeed * dt;
-        } else {
-         std::cout << "plus intersect " << TRANSFORM(zolon)->parent << std::endl;
-            TRANSFORM(zolon)->parent = 0;
-            TRANSFORM(zolon)->position = TRANSFORM(zolon)->worldPosition;
-        }
-        
-        // if we hit a wall -> turn back
-        for (int i=0; i<murs.size(); i++) {
-            if (IntersectionUtil::rectangleRectangle(
-                    TRANSFORM(zolon)->worldPosition, TRANSFORM(zolon)->size, TRANSFORM(zolon)->rotation,
-                    TRANSFORM(murs[i])->position, TRANSFORM(murs[i])->size, TRANSFORM(murs[i])->rotation)) {
-                zolonSpeed = -zolonSpeed;
-                TRANSFORM(zolon)->position.X += 2*zolonSpeed * dt;
-                std::cout << zolonSpeed << std::endl;
-            }
-        }
+    if (BUTTON(startButton)->clicked) {
+        std::cout << "Click" << std::endl;
+        initGame();
     } else {
-        for (int i=0; i<sols.size(); i++) {
-            if (IntersectionUtil::rectangleRectangle(
-                TRANSFORM(zolon)->worldPosition, TRANSFORM(zolon)->size, TRANSFORM(zolon)->rotation,
-                TRANSFORM(sols[i])->position, TRANSFORM(sols[i])->size, TRANSFORM(sols[i])->rotation)) {
-                 std::cout << "youpi " << sols[i] << std::endl;
-                TRANSFORM(zolon)->parent = sols[i];
-                TRANSFORM(zolon)->rotation = 0;
-                TRANSFORM(zolon)->position = Vector2::Rotate(TRANSFORM(zolon)->worldPosition - TRANSFORM(sols[i])->position, -TRANSFORM(sols[i])->rotation);
-                TRANSFORM(zolon)->position.Y = TRANSFORM(zolon)->size.Y * 0.5 + TRANSFORM(sols[i])->size.Y * 0.4;
-                break;
+        if (playing) {
+            TransformationComponent* tc = TRANSFORM(player);
+            if (theTouchInputManager.isTouched()) {
+                const Vector2& newPos = theTouchInputManager.getTouchLastPosition();
+                if (Vector2::Distance(newPos, tc->position) <= tc->size.X) {
+                    tc->position = theTouchInputManager.getTouchLastPosition();
+                }
+                ADSR(obstacles[0])->active = true;
+            }
+            
+            float dt = TimeUtil::getTime() - startTime;
+            std::stringstream a;
+            a << std::fixed << std::setprecision(1) << dt << " s";
+            TEXT_RENDERING(chrono)->text = a.str();
+            
+            float alpha = 1 - ADSR(obstacles[0])->value;
+            for (std::vector<Entity>::iterator it=obstacles.begin(); it!=obstacles.end(); ++it) {
+                Entity e = *it;
+                if (e != arrivee && e != depart) {
+                    RENDERING(*it)->color.a = alpha;
+                    
+                    if (IntersectionUtil::rectangleRectangle(tc, TRANSFORM(*it))) {
+                        RENDERING(e)->color.a = 1;
+                        TEXT_RENDERING(startButton)->text = "Perdu. Rejouer";
+                        obstacleCount -= 10;
+                        resetGame();
+                    }
+                } else if (e == arrivee) {
+                    if (IntersectionUtil::rectangleRectangle(tc, TRANSFORM(*it)) ){
+                        std::cout << "Gagné" << std::endl;
+                        TEXT_RENDERING(startButton)->text = "Gagné. Rejouer";
+                        obstacleCount += 10;
+                        resetGame();
+                    }
+                }
             }
         }
     }
-    
+
+
 
 
     // systems update
