474386d364243d138ead1a2563a0553dcc5fcc3a
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2013-05-05 18:40:03 +0200

    check for collisions with paratroopers

--- sources/systems/BulletSystem.cpp
+++ sources/systems/BulletSystem.cpp
@@ -1,23 +1,27 @@
 #include "BulletSystem.h"
 
+#include "systems/TransformationSystem.h"
 #include "systems/ParatrooperSystem.h"
+
+#include "util/IntersectionUtil.h"
 
 INSTANCE_IMPL(BulletSystem);
 
 BulletSystem::BulletSystem() : ComponentSystemImpl <BulletComponent>("Bullet") {
-    BulletComponent dc;
+    //BulletComponent dc;
 }
 
-void BulletSystem::DoUpdate(float dt) {
+void BulletSystem::DoUpdate(float) {
     FOR_EACH_ENTITY_COMPONENT(Bullet, e, bc)
-        FOR_EACH_ENTITY_COMPONENT(Paratrooper, para, pc)
-
+        FOR_EACH_ENTITY(Paratrooper, para)
+            if (IntersectionUtil::rectangleRectangle(TRANSFORM(e), TRANSFORM(para)))
+                LOGI("collision!");
         }
 	}
 }
 
 #if SAC_INGAME_EDITORS
-void BulletSystem::addEntityPropertiesToBar(Entity entity, TwBar* bar) {
+void BulletSystem::addEntityPropertiesToBar(Entity entity, TwBar*) {
     BulletComponent* dc = Get(entity, false);
     if (!dc) return;
 }
