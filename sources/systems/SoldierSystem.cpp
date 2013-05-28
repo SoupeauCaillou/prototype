02b87575577169c957893b7da2389b13b917d9e7
Jordane Pelloux-Prayer <jordane@damsy.net>
2013-05-28 21:43:21 +0200

    Add soldier system

---
+++ sources/systems/SoldierSystem.cpp
@@ -0,0 +1,35 @@
+/*
+    This file is part of Prototype.
+
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+    @author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Jordane Pelloux-Prayer
+
+    Prototype is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Prototype is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Prototype.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "SoldierSystem.h"
+
+INSTANCE_IMPL(SoldierSystem);
+
+SoldierSystem::SoldierSystem() : ComponentSystemImpl<SoldierComponent>("Soldier") {
+	SoldierComponent sc;
+	componentSerializer.add(new Property<int>("vision_range", OFFSET(visionRange, sc), 0));
+	componentSerializer.add(new Property<int>("attack_range", OFFSET(attackRange, sc), 0));
+	componentSerializer.add(new Property<int>("attack_damage", OFFSET(attackDamage, sc), 0));
+	componentSerializer.add(new Property<int>("p_lance", OFFSET(pLance, sc), 0));
+	componentSerializer.add(new Property<int>("defense_point", OFFSET(defensePoint, sc), 0));
+}
+
+void SoldierSystem::DoUpdate(float) {
+	
+}
---
+++ sources/systems/SoldierSystem.h
@@ -0,0 +1,45 @@
+/*
+    This file is part of Prototype.
+
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+    @author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Jordane Pelloux-Prayer
+
+    Prototype is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Prototype is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Prototype. If not, see <http://www.gnu.org/licenses/>.
+*/
+#pragma once
+
+#include "systems/System.h"
+
+struct SoldierComponent {
+    SoldierComponent() {
+    }
+
+    int visionRange;
+	int attackRange;
+	int attackDamage;
+	int pLance;
+	int defensePoint;
+	
+	enum Actions {
+		Move,
+		Attack,
+	};
+    
+};
+
+#define theSoldierSystem SoldierSystem::GetInstance()
+#define SOLDIER(e) theSoldierSystem.Get(e)
+
+UPDATABLE_SYSTEM(Soldier)
+};
