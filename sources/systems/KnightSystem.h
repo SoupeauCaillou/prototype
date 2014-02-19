71eafd965067270a4a8b64bbd651ab5da02e4036
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-02-19 18:19:57 +0100

    Add Knight system

---
+++ sources/systems/KnightSystem.h
@@ -0,0 +1,43 @@
+/*
+    This file is part of Soupe Au Caillou.
+
+    @author Soupe au Caillou - Jordane Pelloux-Prayer
+    @author Soupe au Caillou - Gautier Pelloux-Prayer
+    @author Soupe au Caillou - Pierre-Eric Pelloux-Prayer
+
+    Soupe Au Caillou is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, version 3.
+
+    Soupe Au Caillou is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Soupe Au Caillou.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+#pragma once
+
+#include "systems/System.h"
+
+
+struct KnightComponent {
+    KnightComponent() : attackRange(1.0f), readyToAttack(false) {}
+
+    float attackRange;
+    bool readyToAttack;
+};
+
+#define theKnightSystem KnightSystem::GetInstance()
+#if SAC_DEBUG
+#define KNIGHT(e) theKnightSystem.Get(e,true,__FILE__,__LINE__)
+#else
+#define KNIGHT(e) theKnightSystem.Get(e)
+#endif
+
+UPDATABLE_SYSTEM(Knight)
+};
