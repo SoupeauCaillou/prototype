8d02143cf3b172a4ec79a0c32e86910c98880084
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-02-20 17:56:27 +0100

    flick: reverse force direction

--- sources/systems/FlickSystem.cpp
+++ sources/systems/FlickSystem.cpp
@@ -76,7 +76,7 @@ void FlickSystem::DoUpdate(float dt) {
                 // apply force
                 fc->status = FlickStatus::Moving; // (transitory state)
                 if (l >= fc->activationDistance.t1) {
-                    float forceMag = - glm::lerp(0.0f, fc->maxForce, (l - fc->activationDistance.t1) / (fc->activationDistance.t2 - fc->activationDistance.t1));
+                    float forceMag = glm::lerp(0.0f, fc->maxForce, (l - fc->activationDistance.t1) / (fc->activationDistance.t2 - fc->activationDistance.t1));
                     PHYSICS(e)->addForce(diff * forceMag / l, glm::vec2(0.0f), 0.016);
                 }
             }
