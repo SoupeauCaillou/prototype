d1deba08cf518e1c4a97763c30201c7301b91188
Jordane Pelloux-Prayer <jordane@damsy.net>
2014-03-19 20:45:04 +0100

    Extract paths contain in xcf files

--- tools/generate_level.py
+++ tools/generate_level.py
@@ -16,15 +16,16 @@ def write_file(filename, data=""):
     f.close()
 
 
-def write_section(sections, section, section_name):
-    sections.append("[{0}]".format(section_name))
+def write_section(section, section_name):
+    sections = ["[{0}]".format(section_name)]
     count = 0
-    for e in section:
+    for element in section:
         count = count + 1
-        sections.append("{0}_position_{1}\t=\t{2}, {3}".format(section_name, count, e[0], e[1]))
-        sections.append("{0}_size_{1}\t=\t{2}, {3}".format(section_name, count, e[2], e[3]))
-        sections.append("{0}_rotation_{1}\t=\t{2}".format(section_name, count, e[4]))
-        sections.append("")
+        for key in element.keys():
+            sections.append("{0}_{1}_{2}\t=\t{3}".format(section_name, key ,count, ', '.join(str(e) for e in element[key])))
+    
+    sections.append("")
+    return sections
 
 
 def parse_file(xcf_filename, directory):
@@ -32,61 +33,75 @@ def parse_file(xcf_filename, directory):
     from gimpfu import gimp
 
     # load xcf file in gimp
-    image = pdb.gimp_file_load(directory + "/" + xcf_filename, xcf_filename)
+    image = pdb.gimp_file_load( os.path.join(directory, xcf_filename), xcf_filename)
 
     sections = []
-    sheep_section = []
-    bush_section = []
-    wall_section = []
-    zone_section = []
-    objectives_section = []
+    # sheep_section = []
+    # bush_section = []
+    # wall_section = []
+    # zone_section = []
+    # objectives_section = []
 
-    current_group = ""
-    #Looking in all layers (included children)
-    for layer in image.layers:
-        current_group = ""
-        for l in [layer] + layer.children:
-            if type(l) is gimp.GroupLayer or l.children != []:
-                print "one more group", l.name
-                current_group = l.name
-                continue
+    # current_group = ""
+    # #Looking in all layers (included children)
+    # for layer in image.layers:
+    #     current_group = ""
+    #     for l in [layer] + layer.children:
+    #         if type(l) is gimp.GroupLayer or l.children != []:
+    #             print "one more group", l.name
+    #             current_group = l.name
+    #             continue
 
-            if "objectifs" in current_group.lower():
-                objectives_section.append(l.name.lower())
-                continue
+    #         if "objectifs" in current_group.lower():
+    #             objectives_section.append(l.name.lower())
+    #             continue
 
-            rotation = 0.0
-            sizeX = l.width
-            sizeY = l.height
-            centerX = l.offsets[0] + sizeX / 2
-            centerY = l.offsets[1] + sizeY / 2
-            if "_" in l.name.lower():
-                rotation = float(l.name.split("_")[-1])
+    #         rotation = 0.0
+    #         sizeX = l.width
+    #         sizeY = l.height
+    #         centerX = l.offsets[0] + sizeX / 2
+    #         centerY = l.offsets[1] + sizeY / 2
+    #         if "_" in l.name.lower():
+    #             rotation = float(l.name.split("_")[-1])
 
-            sizeX = sizeX * cos(radians(rotation))
-            sizeY = sizeY * sin(radians(rotation))
-            t = (centerX, centerY, "{:.2f}".format(sizeX), "{:.2f}".format(sizeY), rotation)
+    #         sizeX = sizeX * cos(radians(rotation))
+    #         sizeY = sizeY * sin(radians(rotation))
+    #         t = { "position": (centerX, centerY), "size": (sizeX, sizeY), "rotation": rotation }
 
-            if "moutons" in current_group.lower():
-                sheep_section.append(t)
-            elif "buisson" in current_group.lower():
-                bush_section.append(t)
-            elif "barriere" in current_group.lower():
-                wall_section.append(t)
-            elif "zone" in current_group.lower():
-                zone_section.append(t)
-            else:
-                print "Warning: Undefined group for layer {0}. This layer" \
-                	" will be ignored.".format(l.name)
+    #         if "moutons" in current_group.lower():
+    #             sheep_section.append(t)
+    #         elif "buisson" in current_group.lower():
+    #             bush_section.append(t)
+    #         elif "barriere" in current_group.lower():
+    #             wall_section.append(t)
+    #         elif "zone" in current_group.lower():
+    #             zone_section.append(t)
+    #         else:
+    #             print("Warning: Undefined group for layer {0}. This layer\
+    #                  will be ignored.".format(l.name) )
+
+    # looking for paths
+    polygon_section = []
+
+    for vector in image.vectors:
+        print vector.strokes
+        for stroke in vector.strokes:              
+            points = {vector.name: map(int, stroke.points[0])}
+            if points not in polygon_section:
+                polygon_section.append( points )
+    
+    # print polygon_section
 
 
-    for o in objectives_section:
-        sections.append(o)
-    write_section(sections, sheep_section, "sheep")
-    write_section(sections, bush_section, "bush")
-    write_section(sections, wall_section, "wall")
-    write_section(sections, zone_section, "zone")
+    # for o in objectives_section:
+    #     sections.append(o)
+    # sections += write_section(sheep_section, "sheep")
+    # sections += write_section(bush_section, "bush")
+    # sections += write_section(wall_section, "wall")
+    # sections += write_section(zone_section, "zone")
+    sections += write_section(polygon_section, "polygon")
     sections.append("")
+    
     return sections
 
 
@@ -94,7 +109,8 @@ def run(filename, directory, output):
 
     start = time.time()
 
-    write_file("level.ini", "\n".join(parse_file(filename, directory)))
+    write_file(os.path.join(directory, output, "level.ini"), "\n".join( parse_file(filename, directory) ) )
+    
     end = time.time()
     print("Finished, total processing time: {:.2f}".format(end - start))
 
@@ -116,8 +132,8 @@ if __name__ == '__main__':
         print("File '{0}' does not have valid extension (.xcf).".format(args.filename))
         sys.exit(2)
 
-    if os.path.exists(args.output + "/level.ini"):
-        print("File '{0}' already exists. Please remove it first.".format(args.output + "/level.ini"))
+    if os.path.exists( os.path.join(args.output, "level.ini") ):
+        print("File '{0}' already exists. Please remove it first.".format( os.path.join(args.output, "level.ini") ) )
         sys.exit(3)
 
     # create output directory
@@ -125,7 +141,7 @@ if __name__ == '__main__':
     if not os.path.isdir(args.output):
         os.mkdir(args.output)
 
-    script_directory = os.path.abspath(os.path.dirname(sys.argv[0]))
+    script_directory = os.path.abspath( os.path.dirname(sys.argv[0]) )
 
     # execute gimp with this script
     os.chdir(script_directory)
