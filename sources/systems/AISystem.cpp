9e0393b5891717d5dd0e5a5b0fe40ff250900b0a
Pierre-Eric Pelloux-Prayer <pierre-eric@damsy.net>
2014-11-16 23:49:21 +0100

    AI shoots on sight

--- sources/systems/AISystem.cpp
+++ sources/systems/AISystem.cpp
@@ -3,6 +3,12 @@
 #include "systems/TransformationSystem.h"
 #include "util/Random.h"
 
+#include "VisibilitySystem.h"
+#include "UnitSystem.h"
+#include "WeaponSystem.h"
+#include "systems/AnchorSystem.h"
+#include "systems/ZSQDSystem.h"
+
 INSTANCE_IMPL(AISystem);
 
 AISystem::AISystem() : ComponentSystemImpl<AIComponent>(HASH("AI", 0x984cbb03)) {
@@ -16,16 +22,47 @@ AISystem::AISystem() : ComponentSystemImpl<AIComponent>(HASH("AI", 0x984cbb03)) 
 
 void AISystem::DoUpdate(float dt) {
     FOR_EACH_ENTITY_COMPONENT(AI, e, uc)
-        if (uc->_pauseAccum > 0) {
+        Entity* weapons = UNIT(e)->weapon;
+
+        uc->_lastSeenAccum += dt;
+
+        if (uc->state == State::Idle && uc->_pauseAccum > 0) {
             uc->_pauseAccum -= dt;
         } else {
             float d = (uc->_targetAngle - TRANSFORM(e)->rotation);
 
             if (glm::abs(d) > 0.01) {
                 TRANSFORM(e)->rotation += glm::sign(d) * glm::min(glm::abs(d), uc->rotationSpeed * dt);
-            } else {
+            } else if (uc->state == State::Idle) {
                 uc->_pauseAccum = uc->pauses.random();
                 uc->_targetAngle = Random::Float(uc->minAngle, uc->maxAngle);
+            }
+        }
+
+        if (uc->state == State::Firing && (uc->_lastSeenAccum > 2)) {
+            for (int i=0; i<2; i++) WEAPON(weapons[i])->fire = false;
+                uc->state = State::Idle;
+                VISIBILITY(e)->raysPerFrame /= 2;
+        }
+
+        // browse visibility results
+        const auto* vc = VISIBILITY(e);
+        for (int i=0; i<vc->visible.count; i++) {
+            Entity visible = vc->visible.entities[i];
+            if (theZSQDSystem.Get(visible, false)) {
+                glm::vec2 diff = TRANSFORM(visible)->position - TRANSFORM(e)->position;
+                uc->_targetAngle = glm::atan(diff.y, diff.x);
+                uc->_lastSeenAccum = 0;
+                if (uc->state == State::Idle) {
+                    uc->state = State::Firing;
+                    VISIBILITY(e)->raysPerFrame *= 2;
+                    WEAPON(weapons[Random::Int(0, 1)])->fire = true;
+                }
+
+                for (int i=0; i<2; i++) {
+                    glm::vec2 diff2 = TRANSFORM(visible)->position - TRANSFORM(weapons[i])->position;
+                    ANCHOR(weapons[i])->rotation = glm::atan(diff2.y, diff2.x) - TRANSFORM(UNIT(e)->body)->rotation;
+                }
             }
         }
     END_FOR_EACH()
--- sources/systems/AISystem.h
+++ sources/systems/AISystem.h
@@ -2,14 +2,24 @@
 
 #include "systems/System.h"
 
+namespace State
+{
+    enum Enum {
+        Idle,
+        Firing,
+    };
+}
+
 struct AIComponent {
-    AIComponent() : minAngle(0), maxAngle(0), rotationSpeed(1), _targetAngle(0) {pauses.t1 = pauses.t2 = 0.0f; _pauseAccum = 0.0f;}
+    AIComponent() : minAngle(0), maxAngle(0), rotationSpeed(1), state(State::Idle), _targetAngle(0), _lastSeenAccum(0) {pauses.t1 = pauses.t2 = 0.0f; _pauseAccum = 0.0f;}
     float minAngle, maxAngle;
     float rotationSpeed;
     Interval<float> pauses;
+    State::Enum state;
 
     float _targetAngle;
     float _pauseAccum;
+    float _lastSeenAccum;
 };
 
 #define theAISystem AISystem::GetInstance()
