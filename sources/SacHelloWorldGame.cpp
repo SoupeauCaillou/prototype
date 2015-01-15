006c7974eab6f423576c8e0b323ec75657c275a7
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2015-01-15 23:45:15 +0100

    Add hexagonal grid

--- sources/SacHelloWorldGame.cpp
+++ sources/SacHelloWorldGame.cpp
@@ -3,9 +3,18 @@
 #include "base/TouchInputManager.h"
 #include "base/Log.h"
 #include "systems/RenderingSystem.h"
+#include "systems/TransformationSystem.h"
+
+SacHelloWorldGame::SacHelloWorldGame() : grid(9,9,1) {
+}
 
 void SacHelloWorldGame::init(const uint8_t*, int) {
-    theEntityManager.CreateEntityFromTemplate("test");
+    grid.forEachCellDo([this] (const GridPos& pos) -> void {
+        Entity e = theEntityManager.CreateEntityFromTemplate("field/cell");
+        RENDERING(e)->color = Color::random();
+        TRANSFORM(e)->position = grid.gridPosToPosition(pos);
+        grid.addEntityAt(e, pos);
+    });
 }
 
 void SacHelloWorldGame::tick(float) {
--- sources/SacHelloWorldGame.h
+++ sources/SacHelloWorldGame.h
@@ -1,10 +1,15 @@
 #pragma once
 #include "base/Game.h"
+#include "util/HexSpatialGrid.h"
 
 class SacHelloWorldGame : public Game {
-public:
+    public:
+        SacHelloWorldGame();
         void init(const uint8_t* in = 0, int size = 0);
 
     private:
         void tick(float dt);
+
+    private:
+        HexSpatialGrid grid;
 };
