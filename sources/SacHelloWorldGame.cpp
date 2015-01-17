e3f862916bf170422ed68e2789d0d6ac1135e437
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2015-01-17 11:27:37 +0100

    Add grass/rock entities

--- sources/SacHelloWorldGame.cpp
+++ sources/SacHelloWorldGame.cpp
@@ -4,16 +4,30 @@
 #include "base/Log.h"
 #include "systems/RenderingSystem.h"
 #include "systems/TransformationSystem.h"
+#include "systems/CameraSystem.h"
 
-SacHelloWorldGame::SacHelloWorldGame() : grid(9,9,1) {
+SacHelloWorldGame::SacHelloWorldGame() : grid(9,9,(glm::sqrt(3.0f) * 0.5f)) {
 }
 
 void SacHelloWorldGame::init(const uint8_t*, int) {
     grid.forEachCellDo([this] (const GridPos& pos) -> void {
-        Entity e = theEntityManager.CreateEntityFromTemplate("field/cell");
+        std::string type = std::string("field/cell_") +( rand()%2?"grass":"rock");
+
+        Entity e = theEntityManager.CreateEntityFromTemplate(type.c_str());
         TRANSFORM(e)->position = grid.gridPosToPosition(pos);
         grid.addEntityAt(e, pos);
     });
+
+     CAMERA(theEntityManager.getEntityByName(HASH("camera",0x526b9e0c)))->clearColor = Color(0,0,0);
+}
+
+bool SacHelloWorldGame::wantsAPI(ContextAPI::Enum api) const {
+	switch (api) {
+		case ContextAPI::Asset:
+			return true;
+		default:
+			return false;
+	}
 }
 
 void SacHelloWorldGame::tick(float) {
--- sources/SacHelloWorldGame.h
+++ sources/SacHelloWorldGame.h
@@ -6,7 +6,7 @@ class SacHelloWorldGame : public Game {
     public:
         SacHelloWorldGame();
         void init(const uint8_t* in = 0, int size = 0);
-
+        bool wantsAPI(ContextAPI::Enum api) const;
     private:
         void tick(float dt);
 
